c538bcd3d489c10dad618cfcbde4dfa2f1d33722
Cliff Seal <cliff@logos-creative.com>
2014-01-03 07:44:00 -0500

    Support excerpts

--- classes/class-woothemes-testimonials.php
+++ classes/class-woothemes-testimonials.php
@@ -103,7 +103,7 @@ class Woothemes_Testimonials {
 			'capability_type' => 'post',
 			'has_archive' => $archive_slug,
 			'hierarchical' => false,
-			'supports' => array( 'title', 'author' ,'editor', 'thumbnail', 'page-attributes' ),
+			'supports' => array( 'title', 'author' ,'editor', 'thumbnail', 'excerpt', 'page-attributes' ),
 			'menu_position' => 5,
 			'menu_icon' => ''
 		);
