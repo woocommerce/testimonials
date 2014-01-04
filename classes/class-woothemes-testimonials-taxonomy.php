4ff47ddc046f22a85efb05fb560225a54acb3f9f
bhubbard <bkhubbard@gmail.com>
2014-01-03 17:48:11 -0800

    Set default slug to /testimonials/category-name/ and set with_front to false.

--- classes/class-woothemes-testimonials-taxonomy.php
+++ classes/class-woothemes-testimonials-taxonomy.php
@@ -89,7 +89,8 @@ class Woothemes_Testimonials_Taxonomy {
 			'show_admin_column' => true,
 			'query_var'         => true,
 			'show_in_nav_menus' => false,
-			'show_tagcloud'     => false
+			'show_tagcloud'     => false,
+			'rewrite' => array( 'slug' => 'testimonials', 'with_front' => false ) );
 		);
 	} // End _get_default_args()
 
